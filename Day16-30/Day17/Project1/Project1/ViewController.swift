0c737376f4f3250248f6cb1b7381e8e7e7731a65
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2022-04-16 19:24:31 +0300

    Use UIImageView, UIImage and  UINavigationBar and customize app

---
+++ Day16-30/Day17/Project1/Project1/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Ömer Faruk Şahin on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        // Big titles on navigation bar
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let pictureName = pictures[indexPath.row]
+        setImageForDetailView(picture: pictureName)
+    }
+    
+    func setImageForDetailView(picture: String) {
+        let detailView = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController
+        if let detail = detailView {
+            detail.selectedImage = picture
+            navigationController?.pushViewController(detail, animated: true)
+        }
+    }
+}
