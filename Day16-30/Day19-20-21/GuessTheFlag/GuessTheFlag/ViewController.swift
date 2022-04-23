2765dd415d61b09eeadf9a1a7ed9fd58e9cae58f
Ömer Faruk Şahin <omerfaruksahiin@gmail.com>
2022-04-23 15:49:19 +0300

    Generate random flags and use UIAlertController, UIAlertAction to inform user

--- Day16-30/Day19-20-21/GuessTheFlag/GuessTheFlag/ViewController.swift
+++ Day16-30/Day19-20-21/GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -14,11 +14,13 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
         button3.layer.borderWidth = 1
@@ -28,15 +30,32 @@ class ViewController: UIViewController {
         button1.layer.borderColor = UIColor.lightGray.cgColor
         
         askQuestion()
-        // Do any additional setup after loading the view.
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var alertTitle = ""
+        
+        if sender.tag == correctAnswer {
+            alertTitle = "Correct!"
+            score += 1
+        } else {
+            score -= 1
+            alertTitle = "Wrong!"
+        }
+        let ac = UIAlertController(title: alertTitle, message: "Score: \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+        
+    }
+    
 }
 
